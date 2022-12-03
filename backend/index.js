2ebe3be53c49eb23640e4029a047d455bce58425
hem193 <hemanthcastel@gmail.com>
2022-12-03 20:01:57 +0530

    Connection established to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Welcome to our todos api...");
 });
 
-app.listen(5000, () => {
-  console.log("server running on port 5000");
+const connection_string = process.env.CONNECTION_STRING;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}...`);
 });
+
+mongoose
+  .connect(connection_string, {
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
