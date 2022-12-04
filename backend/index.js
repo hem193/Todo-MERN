efd79a2ef6ae0557993c5012ea35a5a94220f6f2
hem193 <hemanthcastel@gmail.com>
2022-12-04 12:11:18 +0530

    connection established to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+
 require("dotenv").config();
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to our todos api...");
